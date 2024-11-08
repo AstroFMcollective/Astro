e87c4b11f1b6d61bb845e786ed4164f3927b48a0
sushi <64286083+sushiroles@users.noreply.github.com>
2024-11-08 20:21:33 +0100

    apple music thing

--- AstroAPI/media_services/all.py
+++ AstroAPI/media_services/all.py
@@ -1,6 +1,7 @@
-from AstroAPI.media_services import spotify_api, tidal_api, deezer_api
-from AstroAPI.components.service_auth import service_keys
+from AstroAPI.media_services import spotify_api, apple_music_api, tidal_api, deezer_api
+from AstroAPI.components.ini import service_keys
 
 Spotify = spotify_api.Spotify(client_id = service_keys['spotify']['id'], client_secret = service_keys['spotify']['secret'])
+AppleMusic = apple_music_api.AppleMusic()
 Tidal = tidal_api.Tidal(client_id = service_keys['tidal']['id'], client_secret = f'{service_keys['tidal']['secret']}=')
 Deezer = deezer_api.Deezer()
