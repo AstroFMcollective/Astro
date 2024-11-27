3df8069b7826c9e7f4089f4c7609d0f21901f2f5
sushi <64286083+sushiroles@users.noreply.github.com>
2024-11-27 21:14:51 +0100

    error/empty embeds and explicit tags

--- AstroDiscord/components/embed_composer.py
+++ AstroDiscord/components/embed_composer.py
@@ -11,21 +11,44 @@ def create_anchor(media_object: object):
             anchor.append(text['anchor'][url].replace('URL', urls[url]))
         return '\n'.join(anchor)
     
-def media_embed(command: str, media_object: object, user: object):
-    media_object = media_object
+def create_embed(command: str, media_object: object, user: object):
+    if media_object.type == 'empty_response' or media_object.type == 'error':
+        embed = discord.Embed(
+            title = text['embed']['errortitle'],
+            color = 0xf5c000
+        )
+
+        embed.add_field(
+            name = '',
+            value = text['embed']['errormsg'] if media_object.type == 'error' else text['embed']['emptymsg'],
+            inline = False
+        )
+            
+        embed.set_footer(
+            text = text['embed']['tymsg'],
+            icon_url = text['embed']['pfpurl']
+        )
+
+        return embed
+    
     data = [', '.join(media_object.artists)]
     cover = media_object.cover_url if media_object.type != 'music_video' else media_object.thumbnail_url
+    is_explicit = None
+
     if media_object.type == 'track':
         data.append(media_object.collection)
+        is_explicit = media_object.is_explicit
     elif media_object.type == 'single':
         data.append('Single')
+        is_explicit = media_object.is_explicit
     elif media_object.type == 'music_video':
         data.append('Music Video')
+        is_explicit = media_object.is_explicit
     elif media_object.type == 'album' or media_object.type == 'ep':
         data.append(str(media_object.release_year))
         
     embed = discord.Embed(
-        title = discord.utils.escape_markdown(f'{media_object.title}'),
+        title = f'{discord.utils.escape_markdown(f'{media_object.title}')}  {'`E`' if is_explicit != None and is_explicit != False else ''}',
         description = discord.utils.escape_markdown(f'{' • '.join(data)}'),
         color = image_hex(cover)
     )
