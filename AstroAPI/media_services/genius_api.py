8454c09cfb1d5bf33fe912cf3fe8e2124ad32b49
sushi <64286083+sushiroles@users.noreply.github.com>
2025-01-11 20:28:18 +0100

    genius api support

--- AstroAPI/media_services/genius_api.py
+++ AstroAPI/media_services/genius_api.py
@@ -1,8 +1,6 @@
 from AstroAPI.components import *
 from lyricsgenius import Genius as genius
 
-import aiohttp
-
 
 
 class Genius:
@@ -28,27 +26,68 @@ class Genius:
 
 
 
-	async def search_song(self, artists: list, title: str, song_type: str = None, collection: str = None, is_explicit: bool = None) -> object:
-		request = 'search_song'
-		artists = [optimize_for_search(artist) for artist in artists]
-		title = optimize_for_search(title)
-		collection = clean_up_collection_title(optimize_for_search(collection)) if collection != None else None
-			
-		songs = []
-		start_time = current_unix_time_ms()
-		results = self.api.search_song(title = title, artist = artists[0])
-		for result in results['hits']:
-			song_url = result['result']['url']
-			song_id = result['result']['id']
-			song_title = result['result']['title']
-			song_artists = [result['result']['primary_artist']['name']]
-			song_cover = result['result']['song_art_image_url']
-			song_is_explicit = None
-			song_collection = None
-			end_time = current_unix_time_ms()
-			songs.append(Song(
+	async def search_song(self, artists: list, title: str, song_type: str = None, collection: str = None, is_explicit: bool = None, country_code: str = 'us') -> object:
+		try:
+			request = 'search_song'
+			artists = [optimize_for_search(artist) for artist in artists]
+			title = optimize_for_search(title)
+			collection = clean_up_collection_title(optimize_for_search(collection)) if collection != None else None
+
+			songs = []
+			start_time = current_unix_time_ms()
+			results = self.api.search_songs(f'{artists[0]} {title}')
+			for result in results['hits']:
+				song_url = result['result']['url']
+				song_id = result['result']['id']
+				song_title = result['result']['title']
+				song_artists = [result['result']['primary_artist']['name']]
+				song_cover = result['result']['song_art_image_url']
+				song_is_explicit = None
+				song_collection = None
+				end_time = current_unix_time_ms()
+				songs.append(Song(
+					service = self.service,
+					type = 'track',
+					url = song_url,
+					id = song_id,
+					title = song_title,
+					artists = song_artists,
+					collection = song_collection,
+					is_explicit = song_is_explicit,
+					cover_url = song_cover,
+					api_response_time = end_time - start_time,
+					api_http_code = 200,
+					request = {'request': request, 'artists': artists, 'title': title, 'song_type': song_type, 'collection': collection, 'is_explicit': is_explicit, 'country_code': country_code}
+				))
+			return await filter_song(service = self.service, query_request = request, songs = songs, query_artists = artists, query_title = title, query_song_type = song_type, query_collection = None, query_is_explicit = None)
+		
+		except Exception as msg:
+			error = Error(
 				service = self.service,
-				type = 'track',
+				component = self.component,
+				error_msg = f'Error when searching for song: "{msg}"',
+				request = {'request': request, 'artists': artists, 'title': title, 'song_type': song_type, 'collection': collection, 'is_explicit': is_explicit, 'country_code': country_code}
+			)
+			await log(error)
+			return error
+
+	async def lookup_song(self, id: str, country_code: str = 'us') -> object:
+		try:
+			request = 'lookup_song'
+			start_time = current_unix_time_ms()
+			song = self.api.song(song_id = id)
+			song_type = 'track'
+			song_url = song['song']['url']
+			song_id = song['song']['id']
+			song_title = song['song']['title']
+			song_artists = [song['song']['primary_artist']['name']]
+			song_cover = song['song']['song_art_image_url']
+			song_is_explicit = None
+			song_collection = song['song']['album']['name']
+			end_time = current_unix_time_ms()
+			return Song(
+				service = self.service,
+				type = song_type,
 				url = song_url,
 				id = song_id,
 				title = song_title,
@@ -58,43 +97,15 @@ class Genius:
 				cover_url = song_cover,
 				api_response_time = end_time - start_time,
 				api_http_code = 200,
-				request = {'request': request, 'artists': artists, 'title': title, 'song_type': song_type, 'collection': collection, 'is_explicit': is_explicit}
-			))
-		return await filter_song(service = self.service, query_request = request, songs = songs, query_artists = artists, query_title = title, query_song_type = song_type, query_collection = None, query_is_explicit = None)
-		
+				request = {'request': request, 'id': id, 'country_code': country_code}
+			)
 
-	# TODO: make not slow af
-	async def search_collection(self, artists: list, title: str, year: int = None) -> object:
-		request = 'search_collection'
-		artists = [optimize_for_search(artist) for artist in artists]
-		title = clean_up_collection_title(optimize_for_search(title))
-
-		collections = []
-		start_time = current_unix_time_ms()
-		results = self.api.search_albums(f'{artists[0]} {title}')
-		# print(current_unix_time_ms() - start_time)
-		# save_json(results)
-		for result in results['sections'][0]['hits']:
-			collection_url = result['result']['url']
-			collection_id = result['result']['id']
-			collection_title = result['result']['name']
-			collection_artists = [result['result']['artist']['name']]
-			collection_year = result['result']['release_date_components']['year']
-			collection_cover = result['result']['cover_art_url']
-			end_time = current_unix_time_ms()
-			collections.append(Collection(
+		except Exception as msg:
+			error = Error(
 				service = self.service,
-				type = 'album',
-				url = collection_url,
-				id = collection_id,
-				title = collection_title,
-				artists = collection_artists,
-				release_year = collection_year,
-				cover_url = collection_cover,
-				api_response_time = end_time - start_time,
-				api_http_code = 200,
-				request = {'request': request, 'artists': artists, 'title': title, 'year': year}
-			))
-		return await filter_collection(service = self.service, query_request = request, collections = collections, query_artists = artists, query_title = title, query_year = year)
-
-
+				component = self.component,
+				error_msg = f'Error when looking up song: "{msg}"',
+				request = {'request': request, 'id': id, 'country_code': country_code}
+			)
+			await log(error)
+			return error
