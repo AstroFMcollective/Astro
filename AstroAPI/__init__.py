73ef8cdf7aeb77b2459db91f74868df145322126
sushi <64286083+sushiroles@users.noreply.github.com>
2025-02-03 23:17:27 +0100

    added meta to accessible classes

--- AstroAPI/__init__.py
+++ AstroAPI/__init__.py
@@ -1,2 +1,2 @@
 from AstroAPI.media_services.all import Global, Spotify, AppleMusic, YouTubeMusic, Tidal, Deezer, Genius
-from AstroAPI.components.media import Error, Empty
+from AstroAPI.components.media import Error, Empty, Meta
