71ae6e57e649ad6af4d2cbeeec7a2de192e7f3b6
sushi <64286083+sushiroles@users.noreply.github.com>
2025-10-05 14:40:24 +0200

    Catastrophe logging
    Hastly made function for better reporting of catastrophic errors inside the Astro client

--- AstroDiscord/components/__init__.py
+++ AstroDiscord/components/__init__.py
@@ -3,7 +3,7 @@ from AstroDiscord.components.embed_composer import EmbedComposer
 # from AstroAPI.components.image_hex import image_hex
 # from AstroDiscord.components.url_tools import find_urls, get_regular_url, get_data_from_urls
 from AstroDiscord.components.reactions import add_reactions, check_for_reaction
-from AstroDiscord.components.log import log
+from AstroDiscord.components.log import log, log_catastrophe
 from AstroDiscord.components.time import *
 from AstroDiscord.components.ini import presence, stats
 from AstroDiscord.components.paginator import PaginatorView
--- AstroDiscord/components/log.py
+++ AstroDiscord/components/log.py
@@ -3,7 +3,7 @@ from discord import Webhook
 from AstroDiscord.components.ini import config as discord_config, text as config, tokens as keys
 import aiohttp
 
-async def log(log_embeds: list[discord.Embed], media: list[object], command: str, parameters: str, latency: int, buttons: discord.ui.View):
+async def log(log_embeds: list[discord.Embed], media: list[dict], command: str, parameters: str, latency: int, buttons: discord.ui.View):
 	async with aiohttp.ClientSession() as session:
 		deployment_channel = discord_config['client']['deployment_channel']
 		invalid_responses = [
@@ -58,4 +58,44 @@ async def log(log_embeds: list[discord.Embed], media: list[object], command: str
 			await webhook.send(embeds = embeds, username = 'Astro Client', avatar_url = config['images']['astro_bg'], view = buttons)
 		else:
 			await webhook.send(embeds = embeds, username = 'Astro Client', avatar_url = config['images']['astro_bg'])
+		return
+
+
+
+async def log_catastrophe(command: str, parameters: str, error: str):
+	async with aiohttp.ClientSession() as session:
+		ping = f'<@&1330182469873565749>'
+		deployment_channel = discord_config['client']['deployment_channel']
+		invalid_responses = [
+			'empty_response',
+			'error'
+		]
+
+		embeds = []
+
+		report_type = 'catastrophe'
+
+		report_card = discord.Embed(
+			title = f"Astro Client - `{report_type}`",
+			description = f"Client-side command failed irrecoverably",
+			colour = 0xeb071e,
+		)
+
+		report_card.add_field(
+			name = "Error Report",
+			value = f"Error: `{error}`",
+			inline = False
+		)
+		
+		report_card.add_field(
+			name="Command",
+			value=f"/{command} {parameters}",
+			inline = False
+		)
+
+		embeds.append(report_card)
+		
+		webhook = Webhook.from_url(url = keys['webhooks'][f'{deployment_channel}_logs'], session = session)
+		await webhook.send(ping, embeds = embeds, username = 'Astro Client', avatar_url = config['images']['astro_bg'])
+		print(f'[AstroDiscord] Undocumented error in on_message has occurred: {error}')
 		return
